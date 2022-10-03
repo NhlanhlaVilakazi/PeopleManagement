2ac919de23d325b780d06a5670a63a2607be238e
Nhlanhla Vilakazi <villakazi.nhlanhla@gmail.com>
2022-10-03 03:05:39 +0200

    Amount validation

--- PeopleManagement.ViewModels/Transaction/TransactionViewModel.cs
+++ PeopleManagement.ViewModels/Transaction/TransactionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace PeopleManagement.ViewModels.Transaction
 {
@@ -10,7 +11,11 @@ namespace PeopleManagement.ViewModels.Transaction
         [DisplayName("Transaction Date")]
         public DateTime TransactionDate { get; set; }
         public DateTime CaptureDate { get; set; }
+
+        [Range(1, double.MaxValue, ErrorMessage = "Transaction amount must be greater than 0")]
         public decimal Amount { get; set; }
+
+        [Required]
         public string? Description { get; set; }
     }
 }
